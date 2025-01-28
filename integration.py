c9dbbb5f64df08cda87fab96f75c7294f6407a27
Yunus <ysaid@ualberta.ca>
2025-01-28 16:07:55 -0700

    Added a moving average to reduce arm jerk

--- integration.py
+++ integration.py
@@ -1,3 +1,4 @@
+from collections import deque
 from camera.camera import Camera
 from arm.arm import Arm
 import time
@@ -9,6 +10,9 @@ def main():
     arm.connect()
     print("Camera and Arm connected.")
 
+    # Initialize a deque to store the last 5 coordinates for the moving average
+    coord_history = deque(maxlen=5)
+
     while True:
         ball_position = camera.get_ball_position()
         if ball_position:
@@ -17,9 +21,17 @@ def main():
             x = ball_position[0]
             y = ball_position[2] - 390
             if x >= 200 or x <= -200:
-                y -= 100 # offset beacuse Y gets big as X gets big
-            print(f"Move Net to XY: [{x}, {y}]\n")
-            arm.move_net_to_xy(x_net=x, y_net=y, duration=150, execute=True)
+                y -= 100  # offset because Y gets big as X gets big
+
+            # Append the new coordinates to the deque
+            coord_history.append((x, y))
+
+            # Calculate the moving average of the last 5 coordinates
+            avg_x = int(sum(coord[0] for coord in coord_history) / len(coord_history))
+            avg_y = int(sum(coord[1] for coord in coord_history) / len(coord_history))
+
+            print(f"Move Net to XY (Averaged): [{avg_x}, {avg_y}]\n")
+            arm.move_net_to_xy(x_net=avg_x, y_net=avg_y, duration=150, execute=True)
 
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
